fad786d1bb0854d7cf85034a2f383e2069481f1e
lamnguyen <275nguyenlam@gmail.com>
2021-12-11 13:57:24 +0700

    commit

---
+++ app/src/main/java/com/example/teamproject/model/DrinkModel.java
@@ -0,0 +1,40 @@
+package com.example.teamproject.model;
+
+public class DrinkModel {
+    private String key,Name,image,Price;
+
+    public DrinkModel() {
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        this.Price = price;
+    }
+}
